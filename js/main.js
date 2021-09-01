9cdbef654dc5deac0b2bad97c5bc134656a0bd76
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-01 11:04:00 +0200

    conditional added game

--- js/main.js
+++ js/main.js
@@ -5,31 +5,38 @@ const page = document.querySelector('.js-main');
 const textTrack = document.querySelector('.js-textTrack')
 const button = document.querySelector('.js-button')
 const numberRandom = getRandomNumber();
+const numberAttempts = document.querySelector('.js-numberAttempts')
+let countClick = 0;
 function getRandomNumber() {
     return  Math.ceil(Math.random() * 100);
   
     }
+function addAttempts (){
+countClick += 1;
+}
 
 function guessNumber(event){
     event.preventDefault();
    const inputValue = parseInt(input.value);
-
     console.log(numberRandom)
-     
-     
 
      if( inputValue === numberRandom){
         textTrack.innerHTML = 'Pista: ¡Has ganado Campeona!'
-    
      }
     if ( inputValue < numberRandom){
         textTrack.innerHTML = 'Pista: Demasiado bajo'
     }
     if( inputValue > numberRandom){
-        textTrack.innerHTML = 'Pista: Demasiado Alto'
+        textTrack.innerHTML = 'Pista: Demasiado alto'
+    }
+    if(inputValue <= 0 || inputValue >=100){
+    textTrack.innerHTML = 'Pista: El número debe ser entre 1 y 100'
     }
 
+    addAttempts();
+    
+        
+    
 }
-page
-//window.onload = getRandomNumber;
-button.addEventListener ('click',guessNumber )
+
+button.addEventListener ('click',guessNumber );
