66bc406bd150dbe5953584b87e948ac3632d4541
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-01 11:08:20 +0200

    change JS

--- js/main.js
+++ js/main.js
@@ -1,42 +1,38 @@
-'use strict';
+"use strict";
 
-const input = document.querySelector('.js-input');
-const page = document.querySelector('.js-main');
-const textTrack = document.querySelector('.js-textTrack')
-const button = document.querySelector('.js-button')
+const input = document.querySelector(".js-input");
+const page = document.querySelector(".js-main");
+const textTrack = document.querySelector(".js-textTrack");
+const button = document.querySelector(".js-button");
 const numberRandom = getRandomNumber();
-const numberAttempts = document.querySelector('.js-numberAttempts')
+const numberAttempts = document.querySelector(".js-numberAttempts");
 let countClick = 0;
 function getRandomNumber() {
-    return  Math.ceil(Math.random() * 100);
-  
-    }
-function addAttempts (){
-countClick += 1;
+  return Math.ceil(Math.random() * 100);
+}
+function addAttempts() {
+  countClick += 1;
 }
 
-function guessNumber(event){
-    event.preventDefault();
-   const inputValue = parseInt(input.value);
-    console.log(numberRandom)
+function guessNumber(event) {
+  event.preventDefault();
+  const inputValue = parseInt(input.value);
+  console.log(numberRandom);
 
-     if( inputValue === numberRandom){
-        textTrack.innerHTML = 'Pista: ¡Has ganado Campeona!'
-     }
-    if ( inputValue < numberRandom){
-        textTrack.innerHTML = 'Pista: Demasiado bajo'
-    }
-    if( inputValue > numberRandom){
-        textTrack.innerHTML = 'Pista: Demasiado alto'
-    }
-    if(inputValue <= 0 || inputValue >=100){
-    textTrack.innerHTML = 'Pista: El número debe ser entre 1 y 100'
-    }
+  if (inputValue === numberRandom) {
+    textTrack.innerHTML = "Pista: ¡Has ganado Campeona!";
+  }
+  if (inputValue < numberRandom) {
+    textTrack.innerHTML = "Pista: Demasiado bajo";
+  }
+  if (inputValue > numberRandom) {
+    textTrack.innerHTML = "Pista: Demasiado alto";
+  }
+  if (inputValue <= 0 || inputValue >= 100) {
+    textTrack.innerHTML = "Pista: El número debe ser entre 1 y 100";
+  }
 
-    addAttempts();
-    
-        
-    
+  addAttempts();
 }
 
-button.addEventListener ('click',guessNumber );
+button.addEventListener("click", guessNumber);
